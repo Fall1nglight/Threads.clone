11f6b6d0f217a524a19cff78c314e7fbd790bc2e
Fall1ngLight <keruloszabolcs1@gmail.com>
2026-08-24 20:11:43 +0200

    feat: add follow API client

---
+++ frontend/src/features/follow/followTypes.ts
@@ -0,0 +1,16 @@
+﻿export type FollowUserDto = {
+  id: string
+  username: string
+  isPrivate: boolean
+  createdAtUtc: string
+  followStatusWithCurrentUser?: FollowStatus | null
+}
+
+export enum FollowStatus {
+  Pending = 0,
+  Accepted = 1,
+}
+
+export type FollowStatusResponse = {
+  status: FollowStatus
+}
